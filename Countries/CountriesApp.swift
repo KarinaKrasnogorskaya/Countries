bdb178f3bdab3170607c94e582e700f060770953
KarinaKR00 <kr_karina00@mail.ru>
2023-03-09 16:50:18 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Карина on 09.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
